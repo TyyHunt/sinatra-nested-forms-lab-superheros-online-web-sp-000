ca997d56ce7d1e45b36d2524eaf39fa4eccdc0ce
Tyler Hunt <tyhunt04@gmail.com>
2019-04-10 16:35:31 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,11 +5,11 @@ class App < Sinatra::Base
     set :views, Proc.new { File.join(root, "../views/") }
 
     get '/' do
-      erb 'super_hero'
+      erb :'super_hero'
     end
 
     post '/teams' do
 
-      erb 'team'
+      erb :'team'
     end
 end
